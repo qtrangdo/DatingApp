6b2db856a70189160e2e666eb820f920455cc8de
Trang Do <quynhtrangdo94@gmail.com>
2019-07-11 09:31:23 -0400

    Created navbar component

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ValueComponent } from './components/value/value.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ValueComponent
+    ValueComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
