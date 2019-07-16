a57119383a349e92968900fb55b0943f0ff951ac
Trang Do <quynhtrangdo94@gmail.com>
2019-07-16 16:43:08 -0400

    Switched register form to reactive form

--- DatingApp-SPA/src/app/components/register/register.component.ts
+++ DatingApp-SPA/src/app/components/register/register.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { AuthService } from 'src/app/_services/auth.service';
 import { AlertifyServiceService } from 'src/app/_services/alertify-service.service';
 import { Router } from '@angular/router';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -11,6 +12,7 @@ import { Router } from '@angular/router';
 export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter()
   model: any = {};
+  registerForm: FormGroup;
 
   constructor(
     private authService: AuthService,
@@ -19,15 +21,21 @@ export class RegisterComponent implements OnInit {
   ) { }
 
   ngOnInit() {
+    this.registerForm = new FormGroup({
+      username: new FormControl(),
+      password: new FormControl(),
+      confirmPassword: new FormControl(),
+    })
   }
 
   register() {
-    this.authService.register(this.model).subscribe(() => {
-      this.alertifyService.success('Registered');
-      this.router.navigate(['/'])
-    }, err => {
-      this.alertifyService.error(err)
-    });
+    // this.authService.register(this.model).subscribe(() => {
+    //   this.alertifyService.success('Registered');
+    //   this.router.navigate(['/'])
+    // }, err => {
+    //   this.alertifyService.error(err)
+    // });
+    console.log(this.registerForm.value);
   }
 
   cancel() {
