7a951feec44895c954cb72154e8633cf9ad38258
Trang Do <quynhtrangdo94@gmail.com>
2019-07-16 14:39:58 -0400

    Retrieved user photoUrl when logged in

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -3,6 +3,7 @@ using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
+using AutoMapper;
 using DatingApp.API.Data;
 using DatingApp.API.Dtos;
 using DatingApp.API.Models;
@@ -18,10 +19,12 @@ namespace DatingApp.API.Controllers
   {
     private readonly IAuthRepository _repo;
     private readonly IConfiguration _config;
-    public AuthController(IAuthRepository repo, IConfiguration config)
+    private readonly IMapper _mapper;
+    public AuthController(IAuthRepository repo, IConfiguration config, IMapper mapper)
     {
       this._repo = repo;
       this._config = config;
+      this._mapper = mapper;
     }
 
     [HttpPost("register")]
@@ -68,9 +71,12 @@ namespace DatingApp.API.Controllers
 
       var token = tokenHandler.CreateToken(tokenDescriptor);
 
+      var currentUser = _mapper.Map<UserForListDto>(userFromRepo);
+
       return Ok(new
       {
-        token = tokenHandler.WriteToken(token)
+        token = tokenHandler.WriteToken(token),
+        currentUser
       });
     }
   }
