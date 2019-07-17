5250031880b99d5526c1515396afd342204fa1e2
Trang Do <quynhtrangdo94@gmail.com>
2019-07-17 17:17:28 -0400

    Implemented pagination in API

--- DatingApp.API/Helpers/PageList.cs
+++ DatingApp.API/Helpers/PageList.cs
@@ -24,7 +24,7 @@ namespace DatingApp.API.Helpers
 
         public static async Task<PageList<T>> CreateAsync(IQueryable<T> source, int pageNumber, int pageSize) {
             var count = await source.CountAsync();
-            var items = await source.Skip((pageNumber - 1) * pageNumber).Take(pageSize).ToListAsync();
+            var items = await source.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToListAsync();
             return new PageList<T>(items, count, pageNumber, pageSize);
         }
     }
