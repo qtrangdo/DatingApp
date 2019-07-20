819a7bdd3702803c2583bd52174a258a619bffc7
Trang Do <quynhtrangdo94@gmail.com>
2019-07-20 17:28:15 -0400

    Mapped messages for return

--- DatingApp.API/Controllers/MessagesController.cs
+++ DatingApp.API/Controllers/MessagesController.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using AutoMapper;
@@ -38,6 +39,20 @@ namespace DatingApp.API.Controllers
       return Ok(messageFromRepo);
     }
 
+    [HttpGet]
+    public async Task<IActionResult> GetMessagesForUser(int userId, [FromQuery] MessageParams messageParams)
+    {
+      if (userId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+      {
+        return Unauthorized();
+      }
+      messageParams.UserId = userId;
+      var messagesFromRepo = await _repo.GetMessagesForUser(messageParams);
+      var messages = _mapper.Map<IEnumerable<MessageToReturnDto>>(messagesFromRepo);
+      Response.AddPagination(messagesFromRepo.CurrentPage, messagesFromRepo.PageSize, messagesFromRepo.TotalCount, messagesFromRepo.TotalPages);
+      return Ok(messages);
+    }
+
     [HttpPost]
     public async Task<IActionResult> CreateMessage(int userId, MessageForCreationDto messageForCreation)
     {
@@ -45,19 +60,20 @@ namespace DatingApp.API.Controllers
       {
         return Unauthorized();
       }
-        messageForCreation.SenderId = userId;
+      messageForCreation.SenderId = userId;
 
-        var recipient = await _repo.GetUser(messageForCreation.RecipientId);
-        if (recipient == null) return BadRequest("Could not find user");
+      var recipient = await _repo.GetUser(messageForCreation.RecipientId);
+      if (recipient == null) return BadRequest("Could not find user");
 
-        var message = _mapper.Map<Message>(messageForCreation);
-        _repo.Add(message);
-        var messageToReturn = _mapper.Map<MessageForCreationDto>(message);
+      var message = _mapper.Map<Message>(messageForCreation);
+      _repo.Add(message);
+      var messageToReturn = _mapper.Map<MessageForCreationDto>(message);
 
-        if (await _repo.SaveAll()) {
-            return CreatedAtRoute("GetMessage", new {id = message.Id}, messageToReturn);
-        }
-        throw new Exception("Failed to save created message");
+      if (await _repo.SaveAll())
+      {
+        return CreatedAtRoute("GetMessage", new { id = message.Id }, messageToReturn);
+      }
+      throw new Exception("Failed to save created message");
     }
   }
 }
