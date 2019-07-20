98f77136906eccc3442d5c9e656f875f8a5545b6
Trang Do <quynhtrangdo94@gmail.com>
2019-07-20 17:40:47 -0400

    Added GetMessageThread method

--- DatingApp.API/Data/DatingRepository.cs
+++ DatingApp.API/Data/DatingRepository.cs
@@ -117,9 +117,16 @@ namespace DatingApp.API.Data
       throw new NotImplementedException();
     }
 
-    public Task<IEnumerable<Message>> GetMessageThread(int userId, int recipientId)
+    public async Task<IEnumerable<Message>> GetMessageThread(int userId, int recipientId)
     {
-      throw new NotImplementedException();
+      var messages = await _context.Messages.Include(u => u.Sender).ThenInclude(p => p.Photos)
+        .Include(u => u.Recipient).ThenInclude(p => p.Photos)
+        .Where(m => m.RecipientId == userId && m.SenderId == recipientId
+         || m.SenderId == userId && m.RecipientId == recipientId)
+        .OrderByDescending(m => m.MessageSent)
+        .ToListAsync();
+
+      return messages;
     }
 
     public async Task<PageList<Message>> GetMessagesForUser(MessageParams messageParams)
