24837e4630a515fa148c926866552c70fe3554b1
Trang Do <quynhtrangdo94@gmail.com>
2019-07-22 15:47:23 -0400

    Added deleting message functionality

--- DatingApp.API/Data/DatingRepository.cs
+++ DatingApp.API/Data/DatingRepository.cs
@@ -121,8 +121,8 @@ namespace DatingApp.API.Data
     {
       var messages = await _context.Messages.Include(u => u.Sender).ThenInclude(p => p.Photos)
         .Include(u => u.Recipient).ThenInclude(p => p.Photos)
-        .Where(m => m.RecipientId == userId && m.SenderId == recipientId
-         || m.SenderId == userId && m.RecipientId == recipientId)
+        .Where(m => m.RecipientId == userId && m.RecipientDelete == false && m.SenderId == recipientId
+         || m.SenderId == userId && m.SenderDeleted == false && m.RecipientId == recipientId)
         .OrderByDescending(m => m.MessageSent)
         .ToListAsync();
 
@@ -137,13 +137,13 @@ namespace DatingApp.API.Data
       switch (messageParams.MessageContainer)
       {
         case "Inbox":
-          messages = messages.Where(u => u.RecipientId == messageParams.UserId);
+          messages = messages.Where(u => u.RecipientId == messageParams.UserId && u.RecipientDelete == false);
           break;
         case "Outbox":
-          messages = messages.Where(u => u.SenderId == messageParams.UserId);
+          messages = messages.Where(u => u.SenderId == messageParams.UserId && u.SenderDeleted == false);
           break;
         default:
-          messages = messages.Where(u => u.RecipientId == messageParams.UserId && u.IsRead == false);
+          messages = messages.Where(u => u.RecipientId == messageParams.UserId && u.RecipientDelete == false && u.IsRead == false);
           break;
       }
 
