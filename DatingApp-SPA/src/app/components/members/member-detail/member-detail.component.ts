a66f97fa34a2d69d0f14b90cbd58ae60a79ed1c7
Trang Do <quynhtrangdo94@gmail.com>
2019-07-22 14:03:58 -0400

    Added query params to angular route

--- DatingApp-SPA/src/app/components/members/member-detail/member-detail.component.ts
+++ DatingApp-SPA/src/app/components/members/member-detail/member-detail.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { User } from 'src/app/_models/User';
 import { UserService } from 'src/app/_services/user.service';
 import { AlertifyServiceService } from 'src/app/_services/alertify-service.service';
 import { ActivatedRoute } from '@angular/router';
 import { NgxGalleryOptions, NgxGalleryImage, NgxGalleryAnimation } from 'ngx-gallery';
+import { TabsetComponent } from 'ngx-bootstrap/tabs/public_api';
 
 @Component({
   selector: 'app-member-detail',
@@ -11,6 +12,7 @@ import { NgxGalleryOptions, NgxGalleryImage, NgxGalleryAnimation } from 'ngx-gal
   styleUrls: ['./member-detail.component.css']
 })
 export class MemberDetailComponent implements OnInit {
+  @ViewChild('memberTabs', {static: true}) memberTabs: TabsetComponent
   user: User;
   galleryOptions: NgxGalleryOptions[];
   galleryImages: NgxGalleryImage[];
@@ -25,6 +27,11 @@ export class MemberDetailComponent implements OnInit {
     this.route.data.subscribe(data => {
       this.user = data['user']
     }, err => this.alertify.error(err));
+
+    this.route.queryParams.subscribe(params => {
+      const selectedTab = params['tab'];
+      this.memberTabs.tabs[selectedTab > 0 ? selectedTab : 0].active = true;
+    });
 
     this.galleryOptions = [
       {
@@ -53,11 +60,7 @@ export class MemberDetailComponent implements OnInit {
     return imgUrls
   }
 
-  // loadUser() {
-  //   const id = +this.route.snapshot.params['id'];
-  //   // '+' is used to convert id from string to number
-  //   this.userService.getUser(id).subscribe(user => this.user = user,
-  //     err => this.alertify.error(err)
-  //   );
-  // }
+  selectTab(tabId: number) {
+    this.memberTabs.tabs[tabId].active = true;
+  }
 }
