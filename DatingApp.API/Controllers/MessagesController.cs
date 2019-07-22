bd1f705629e434ab619cdb0c5b8fa264c342ecc1
Trang Do <quynhtrangdo94@gmail.com>
2019-07-22 16:22:54 -0400

    Marked read messages API

--- DatingApp.API/Controllers/MessagesController.cs
+++ DatingApp.API/Controllers/MessagesController.cs
@@ -111,5 +111,23 @@ namespace DatingApp.API.Controllers
 
       throw new Exception("Error deleting the message");
     }
+
+    [HttpPost("{id}/read")]
+    public async Task<IActionResult> MarkMessageAsRead(int userId, int id) {
+      if (userId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+      {
+        return Unauthorized();
+      }
+      
+      var messageFromRepo = await _repo.GetMessage(id);
+      if (messageFromRepo.RecipientId != userId) return Unauthorized();
+
+      messageFromRepo.IsRead = true;
+      messageFromRepo.DateRead = DateTime.Now;
+
+      await _repo.SaveAll();
+
+      return NoContent();
+    }
   }
 }
