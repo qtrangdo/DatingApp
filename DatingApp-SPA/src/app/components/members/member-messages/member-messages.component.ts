abe705a0c8069ac4efd65d033032c2bdacdd33b4
Trang Do <quynhtrangdo94@gmail.com>
2019-07-22 16:22:21 -0400

    Marked read messages

--- DatingApp-SPA/src/app/components/members/member-messages/member-messages.component.ts
+++ DatingApp-SPA/src/app/components/members/member-messages/member-messages.component.ts
@@ -3,6 +3,7 @@ import { Message } from 'src/app/_models/Message';
 import { UserService } from 'src/app/_services/user.service';
 import { AuthService } from 'src/app/_services/auth.service';
 import { AlertifyServiceService } from 'src/app/_services/alertify-service.service';
+import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-member-messages',
@@ -25,12 +26,22 @@ export class MemberMessagesComponent implements OnInit {
   }
 
   loadMessages() {
-    this.userService.getMessageThread(this.authService.decodeToken.nameid, this.recipientId)
+    const currentUserId = +this.authService.decodeToken.nameid
+    this.userService.getMessageThread(currentUserId, this.recipientId)
+      .pipe(
+        tap(messages => {
+          for (let i = 0; i < messages.length; i++) {
+            if (messages[i].isRead === false && messages[i].recipientId === currentUserId) {
+              this.userService.markAsRead(currentUserId, messages[i].id);
+            }
+          }
+        })
+      )
       .subscribe(messages => this.messages = messages,
         err => this.alertify.error(err)
       )
   }
-  
+
   sendMessage() {
     this.newMessage.recipientId = this.recipientId;
     this.userService.sendMessage(this.authService.decodeToken.nameid, this.newMessage).subscribe((message: Message) => {
